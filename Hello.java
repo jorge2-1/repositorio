159e50eddcaf9277dcfbbcfb818493e1904cb9ba
jorge <Leongar007@gmail.com>
2025-08-06 09:44:47 -0500

    first commit

---
+++ Hello.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        int x, y;
+        x = 3;
+        y = 2;
+
+        System.out.println("Hola mundo");
+        System.out.println();
+        System.out.println("La suma es: " + (x + y));
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Escribe tu nombre: ");  // <-- mensaje antes del input
+        String nombre = sc.nextLine();
+
+        System.out.println("Hola " + nombre);
+        sc.close();
+    }
+}
